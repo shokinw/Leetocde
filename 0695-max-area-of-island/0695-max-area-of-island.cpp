c0b2c62ddea286171e09903f127676466931b891
Samridhi Sharma <148897055+shokinw@users.noreply.github.com>
2026-07-24 14:52:36 +0530

    Time: 0 ms (100%), Space: 27.4 MB (91.23%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        // Base Case
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0)
+            return 0;
+
+        // Mark as visited
+        grid[i][j] = 0;
+
+        // Current cell + 4 directions
+        return 1 +
+               dfs(grid, i + 1, j) +
+               dfs(grid, i - 1, j) +
+               dfs(grid, i, j + 1) +
+               dfs(grid, i, j - 1);
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int maxArea = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    int area = dfs(grid, i, j);
+                    maxArea = max(maxArea, area);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+};
