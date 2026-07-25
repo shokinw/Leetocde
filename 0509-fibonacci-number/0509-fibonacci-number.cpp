6170e881794e06a8bc49ed37220521a214a6400c
Samridhi Sharma <148897055+shokinw@users.noreply.github.com>
2026-07-25 22:26:46 +0530

    Time: 13 ms (31.78%), Space: 7.8 MB (38.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,14 +4,7 @@ public:
         if(n<=1){
             return n;
         }
-        int prev1=0;
-        int prev2=1;
-
-        for(int i=2;i<=n;i++){
-            int current=prev1+prev2;
-            prev1=prev2;
-            prev2=current;
-        }
-    return prev2;  
+        return fib(n-1)+fib(n-2);
+        
     }
 };
