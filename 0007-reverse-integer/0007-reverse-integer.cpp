7229e3debf889fce9c81f20d8d86a789bc0eeee5
Samridhi Sharma <148897055+shokinw@users.noreply.github.com>
2026-07-26 22:50:39 +0530

    Time: 6 ms (6.43%), Space: 8.6 MB (21.22%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while(x != 0) {
+
+            int digit = x % 10;
+            x = x / 10;
+
+            // overflow check
+            if(rev > INT_MAX/10 || 
+              (rev == INT_MAX/10 && digit > 7))
+                return 0;
+
+            // underflow check
+            if(rev < INT_MIN/10 || 
+              (rev == INT_MIN/10 && digit < -8))
+                return 0;
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+    }
+};
